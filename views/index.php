87e080fcfa1e6c1a5bfc9a06cde6414f969707ba
Nicole Marinho Espíndola <nicole.m.espindola@gmail.com>
2023-08-23 23:11:23 -0300

    adding 'home' green block

--- views/index.php
+++ views/index.php
@@ -17,3 +17,12 @@
         <img class="join-us-img" src="./../assets/img/Taking care of the Earth-amico.png" alt="">
     </div>
 </section>
+<section class="green-block-explain">
+    <div class="green-title-block">
+        <img src="../assets/img/logo-oasis-verde.png" alt="" class="green-block-img">
+        <span class="green-block-title">Imagine um Oásis....</span>
+    </div>
+    <p class="green-block-text">
+    A Oásis é uma comunidade de voluntários dedicados a preservar e restaurar a natureza. Junte-se a nós para fazer a diferença e ajudar a criar um mundo mais verde e sustentável.
+    </p>
+</section>
