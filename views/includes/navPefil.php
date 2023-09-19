64ce4d9b6274ff3c7fbd7128ea990dc4161eac61
Nicole Marinho Espíndola <nicole.m.espindola@gmail.com>
2023-09-18 23:44:28 -0300

    adding some things on volunteer profile

---
+++ views/includes/navPefil.php
@@ -0,0 +1,37 @@
+<?php
+    function includeURL($path = '') {
+        return sprintf(
+            "%s/%s/%s",
+            $_SERVER['DOCUMENT_ROOT'],
+            'Oasis',
+            $path
+        );
+    }
+
+    include_once(includeURL('/services/helpers.php'));
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/4719b1c3ae.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= baseUrl('/assets/css/global.css')?>">
+    <link rel="stylesheet" href="<?= baseUrl('/assets/css/navbar.css')?>">
+    <link rel="stylesheet" href="<?= baseUrl('/assets/css/perfil.css')?>">
+    <title>Perfil voluntario | Oásis</title>
+</head>
+
+<nav class="nav-perfil">
+    <div class="back-block">
+        <a href="<?= baseUrl('/index.php') ?>" class="back-green-btn">
+            <i class="fa-solid fa-chevron-left"></i>
+        </a>
+    </div>
+    <div class="logo-block">
+        <img class="logo" src="<?= baseUrl('/assets/img/logo-oasis.png') ?>" alt="Logo da Oásis">
+        <span class="logo-nome">Oásis</span>
+    </div>
+    <div class="btn-block">
+        <button class="">Sair</button>
+    </div>
+</nav>
